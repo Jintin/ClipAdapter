749ccccfd14bf80c26c561c8298508253bf860e6
Jintin <jintin.lin1018@gmail.com>
2020-11-30 20:06:37 +0800

    init

---
+++ app/src/main/java/com/jintin/clipadapter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jintin.clipadapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
